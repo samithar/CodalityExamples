9a39c1b8b6f00d3eb636abaf710a45df77d9f275
samithar <samitharansara@gmail.com>
2021-04-23 08:50:03 +1200

    TapeEquilibrium performance not good enough

--- TapeEquilibrium.cpp
+++ TapeEquilibrium.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<numeric>
+#include<algorithm>
 using namespace std;
 
 int solution(vector<int> &A) {
 
+	vector<int> p,n,diff;
+	for (int i = 1; i < (int)A.size(); ++i) {
+		n =vector<int>(A.begin() + i, A.end());
+		p= vector<int>(A.begin(), (A.end()-(A.size()-i)));
+		int sum_p =accumulate(n.begin(), n.end(), 0);
+		int sum_n=accumulate(p.begin(), p.end(), 0);
+		int abs_diff = abs(sum_n - sum_p);
+		diff.push_back(abs_diff);
+		
+	}
+	return *min_element(diff.begin(), diff.end());
 }
 int main(void) {
 	vector<int> test{ 3,1,2,4,3 };
-	solution(test);
+	cout<<solution(test)<<endl;
 
 }
