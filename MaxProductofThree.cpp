0296029d497ffa3a2f9c1a42642068ed241eaac4
samithar <samitharansara@gmail.com>
2021-04-23 16:28:41 +1200

    redundant condition

--- MaxProductofThree.cpp
+++ MaxProductofThree.cpp
@@ -11,9 +11,9 @@ int solution(vector<int>& A) {
 	Res.push_back(A[0] * A[1] * A[2]);
 	Res.push_back(A[A.size()-1] * A[A.size() - 2] * A[A.size() - 3]);
 	Res.push_back(A[0] * A[1] * A[A.size() - 1]);
-	Res.push_back(A[0] * A[A.size() - 1] * A[A.size() - 2]);
+	//Res.push_back(A[0] * A[A.size() - 1] * A[A.size() - 2]);
 	sort(Res.begin(), Res.end());
-	return Res[3];
+	return Res[2];
 }
 int main(void) {
 	vector<int> test{ -3,1,2,-2,5,6 };
